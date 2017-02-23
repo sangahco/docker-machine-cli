bd5adf4a4830ee387b747645e92390c8d6fd370e
Emanuele Disco <emawind84@gmail.com>
2017-02-23 11:20:25 +0900

    update

--- cleanup.sh
+++ cleanup.sh
@@ -3,3 +3,6 @@
 docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
 docker ps --filter status=dead --filter status=exited --filter status=created  -aq | xargs docker rm -v
 docker volume rm $(docker volume ls -q)
+
+# remove all images
+docker rmi -f $(docker images | awk "{print \$3}")
