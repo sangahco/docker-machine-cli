eaabff18cfdd9ac26e4c4aeb29ade27d2accfc98
Emanuele Disco <emawind84@gmail.com>
2017-03-31 17:18:53 +0900

    update

--- prepare-ami.sh
+++ prepare-ami.sh
@@ -2,10 +2,7 @@
 
 sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
 
-sudo shred -u ~/.ssh/authorized_keys
 shred -u ~/.ssh/authorized_keys
-
-sudo shred -u ~/.*history
 shred -u ~/.*history
 
 sudo apt-get clean
