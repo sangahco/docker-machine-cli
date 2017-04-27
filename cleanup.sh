cda39017e7adb164212ba22ae8194ea16fdfca63
Emanuele Disco <emawind84@gmail.com>
2017-04-27 19:23:58 +0900

    added remove all docker logs to cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+DOCKER_HOME=/var/lib/docker
+
 docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
 docker ps --filter status=dead --filter status=exited --filter status=created  -aq | xargs docker rm -v
 docker volume rm $(docker volume ls -q)
@@ -9,3 +11,6 @@ docker rmi -f $(docker images | awk "{print \$3}")
 
 # remove all networks
 docker network rm $(docker network ls --format {{.ID}})
+
+# truncate all docker logs
+truncate -s 0 $DOCKER_HOME/containers/*/*-json.log
