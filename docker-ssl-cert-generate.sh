f7bceb07b894dfa59f2dcf5ae3d3912f0bc39817
Emanuele Disco <emawind84@gmail.com>
2017-12-05 14:54:05 +0900

    update

--- docker-ssl-cert-generate.sh
+++ docker-ssl-cert-generate.sh
@@ -2,24 +2,32 @@
 
 # HEADS UP! Make sure to use '*' or a valid hostname for the FDQN prompt
 
-echo 01 > ca.srl
+#echo 01 > ca.srl
 openssl genrsa -des3 -out ca-key.pem
-openssl req -new -x509 -days 365 -key ca-key.pem -out ca.pem
+openssl req -new -x509 -days 365 -key ca-key.pem -out ca-cert.pem
 
+# server key/certificate
 openssl genrsa -des3 -out server-key.pem
 openssl req -new -key server-key.pem -out server.csr
+openssl x509 -req -days 365 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -out server-cert.pem
 
-openssl x509 -req -days 365 -in server.csr -CA ca.pem -CAkey ca-key.pem -out server-cert.pem
-
+# client key/certificate
 openssl genrsa -des3 -out client-key.pem
 openssl req -new -key client-key.pem -out client.csr
-
 echo extendedKeyUsage = clientAuth > extfile.cnf
+openssl x509 -req -days 365 -in client.csr -CA ca-cert.pem -CAkey ca-key.pem -out client-cert.pem -extfile extfile.cnf
 
-openssl x509 -req -days 365 -in client.csr -CA ca.pem -CAkey ca-key.pem -out client-cert.pem -extfile extfile.cnf
-
+# remove passphrase from private key files if present
 openssl rsa -in server-key.pem -out server-key.pem
 openssl rsa -in client-key.pem -out client-key.pem
+
+rm extfile.cnf
+rm ca.srl
+rm client.csr
+rm server.csr
+
+
+
 
 # server
 # sudo docker -d --tlsverify --tlscacert=ca.pem --tlscert=server-cert.pem --tlskey=server-key.pem -H=0.0.0.0:4243
