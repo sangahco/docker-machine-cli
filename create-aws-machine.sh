bc237e5335c8748f648e1ac6865e74cc70601ce4
Emanuele Disco <emawind84@gmail.com>
2022-09-21 18:17:44 +0900

    - improved create-aws-machine script

--- create-aws-machine.sh
+++ create-aws-machine.sh
@@ -20,14 +20,19 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
-AWS_INSTANCE_TYPE=${AWS_INSTANCE_TYPE:-t3.small}
+AWS_INSTANCE_TYPE=${AWS_INSTANCE_TYPE:-t2.small}
 if [ "$2" != "" ]; then
     AWS_INSTANCE_TYPE="$2"
 fi
 
-MACHINE_IMAGE=${MACHINE_IMAGE:-ami-01ab73f5311a53a55}
+MACHINE_IMAGE=${MACHINE_IMAGE:-ami-05a7435a08406e458}
 if [ "$3" != "" ]; then
     MACHINE_IMAGE="$3"
+fi
+
+AWS_SSH_USER=${AWS_SSH_USER:-ec2-user}
+if [ "$4" != "" ]; then
+    AWS_SSH_USER="$4"
 fi
 
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-ap-northeast-2}
@@ -47,5 +52,6 @@ docker-machine create \
 --amazonec2-secret-key ${AWS_SECRET_ACCESS_KEY} \
 --amazonec2-ami "${MACHINE_IMAGE}" \
 --amazonec2-instance-type ${AWS_INSTANCE_TYPE} \
+--amazonec2-ssh-user ${AWS_SSH_USER} \
 --amazonec2-root-size 50 \
 ${MACHINE_NAME}
